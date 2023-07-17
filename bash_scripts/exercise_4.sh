e34483d3d0a804439a1afbab9c333a87e4341202
magdalena-w <walesa.magdalena@gmail.com>
2023-07-17 14:52:08 +0200

    Fixed error with unused values

--- bash_scripts/exercise_4.sh
+++ bash_scripts/exercise_4.sh
@@ -6,21 +6,17 @@ a=abcdefghijklmnopqrstuvwxyz
 b=ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 function print_usage() {
-    echo "usage: $0 [-s shift_value] [-i input_file] [-o output_file]"
+    echo "usage: $0 [-s shift_value_value] [-i input_file] [-o output_file]"
 }
 
 function encrypt() {
-    sed "y/$a$b/${a:$shift}${a::$shift}${b:$shift}${b::$shift}/" input_file > output_file
+    sed "y/$a$b/${a:$shift_value}${a::$shift_value}${b:$shift_value}${b::$shift_value}/" "$input_file" > "$output_file"
 }
 
-shift=''
-input_file=''
-output_file=''
-
-while getopts ':s:i:o' flag; do
+while getopts 's:i:o:' flag; do
     case "${flag}" in
-        s)  # handle shift
-            shift="${OPTARG}"
+        s)  # handle shift_value
+            shift_value="${OPTARG}"
             ;;
         i)  # handle input file
             input_file="${OPTARG}"
@@ -36,8 +32,3 @@ while getopts ':s:i:o' flag; do
 done
 
 encrypt
-
-# Debug output
-echo ""
-echo "All ARGS: ${@}"
-
