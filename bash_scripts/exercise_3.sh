406d1727993176196010920d3308bb729d63fd7b
magdalena-w <walesa.magdalena@gmail.com>
2023-07-17 11:52:19 +0200

    Added FizzBuzz script

---
+++ bash_scripts/exercise_3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function fizzbuzz {
+    for num in {1..100}
+    do
+        if [ $(( $num % 15 )) -eq 0 ]; then
+            echo "FizzBuzz"
+        elif [ $(( $num % 3 )) -eq 0 ]; then
+            echo "Fizz"
+        elif [ $(( $num % 5 )) -eq 0 ]; then
+            echo "Buzz"
+        else
+            echo $num
+        fi
+    done
+}
+fizzbuzz
