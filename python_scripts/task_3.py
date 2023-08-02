59ffe9628a1b55c115214411e544508d9dca98e7
magdalena-w <walesa.magdalena@gmail.com>
2023-08-02 15:38:17 +0200

    Implementation of task 3

--- python_scripts/task_3.py
+++ python_scripts/task_3.py
@@ -9,5 +9,22 @@ Author:
         magdalena-w
 
 """
+import argparse
+from collections import defaultdict
 
+parser = argparse.ArgumentParser(description='Provides total number of different User Agents and some statistics')
+parser.add_argument('-f', '--file', help='Name of the access log file')
+args = parser.parse_args()
 
+user_agents = defaultdict(int)
+
+with open(args.file, 'r') as file:
+    for line in file:
+        # Assuming that file is in specific format
+        user_agent = line.split('"')[5]
+        user_agents[user_agent] += 1
+
+print(f"Number of different user agents: {len(user_agents)}\n")
+print(f"Statistics about every User Agent:")
+for user_agent, requests in user_agents.items():
+    print(f"User Agent: \n{user_agent}\nNumber of requests: {requests}\n")
